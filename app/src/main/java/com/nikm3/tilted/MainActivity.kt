d706b235eaf899e0c476b2707fdd554b2c44b8f6
NikM3 <nikolas.a.melendez@gmail.com>
2023-03-28 18:49:51 -0400

    Initial commit

---
+++ app/src/main/java/com/nikm3/tilted/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nikm3.tilted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
