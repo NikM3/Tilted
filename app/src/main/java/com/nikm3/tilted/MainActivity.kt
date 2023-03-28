c6f51eadef5f681254a1b7012d2df6297ee89957
NikM3 <nikolas.a.melendez@gmail.com>
2023-03-28 19:28:51 -0400

    Starter app built

--- app/src/main/java/com/nikm3/tilted/MainActivity.kt
+++ app/src/main/java/com/nikm3/tilted/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.nikm3.tilted
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nikm3.tilted.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorAccelerometer: Sensor
+    private lateinit var sensorMagnetometer: Sensor
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set sensors
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorMagnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Set binding
+        val goalDirection = binding.goalDirection
+        val goalPitch = binding.goalPitch
+        val goalRoll = binding.goalRoll
+        val playerDirection = binding.playerDirection
+        val playerPitch = binding.playerPitch
+        val playerRoll = binding.playerRoll
+
+        sensorManager.registerListener(
+            this, sensorAccelerometer,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            this, sensorMagnetometer,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
     }
 }
