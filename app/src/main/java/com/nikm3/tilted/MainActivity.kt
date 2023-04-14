32bc5b1ac678b3a2003df728a6d4e163e7ea57b1
NikM3 <nikolas.a.melendez@gmail.com>
2023-04-14 19:26:14 -0400

    Basic gameplay done

--- app/src/main/java/com/nikm3/tilted/MainActivity.kt
+++ app/src/main/java/com/nikm3/tilted/MainActivity.kt
@@ -1,130 +1,160 @@
 package com.nikm3.tilted
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.Display
-import android.view.Surface
-import android.view.WindowManager
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.rotationMatrix
-import com.nikm3.tilted.databinding.ActivityMainBinding
+import android.util.DisplayMetrics
+import android.view.View
+import kotlin.random.Random
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var display: Display
-    private lateinit var sensorManager: SensorManager
-    private lateinit var sensorAccelerometer: Sensor
-    private lateinit var sensorMagnetometer: Sensor
-    private lateinit var playerDirection: TextView
-    private lateinit var playerPitch: TextView
-    private lateinit var playerRoll: TextView
-    private var accelerometerData = FloatArray(3)
-    private var magnetometerData = FloatArray(3)
-
+class MainActivity : Activity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+    private var accelerometer: Sensor? = null
+    var animatedView: AnimatedView? = null
+    var puckDrawable = ShapeDrawable()
+    var goalDrawable = ShapeDrawable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // setContentView(R.layout.activity_main);
 
-        // Set sensors
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        sensorMagnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
 
-        // Prepare to lock orientation
-        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
-        display = windowManager.defaultDisplay
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        max_x = displayMetrics.widthPixels
+        max_y = displayMetrics.heightPixels
+        puckX = max_x / 2
+        puckY = max_y / 2
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager!!
+            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        animatedView = AnimatedView(this)
+        setContentView(animatedView)
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        // Set binding
-        val goalDirection = binding.goalDirection
-        val goalPitch = binding.goalPitch
-        val goalRoll = binding.goalRoll
-        playerDirection = binding.playerDirection
-        playerPitch = binding.playerPitch
-        playerRoll = binding.playerRoll
-
-        sensorManager.registerListener(
-            this, sensorAccelerometer,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-        sensorManager.registerListener(
-            this, sensorMagnetometer,
-            SensorManager.SENSOR_DELAY_NORMAL
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(
+            this, accelerometer,
+            SensorManager.SENSOR_DELAY_GAME
         )
     }
 
-    override fun onStop() {
-        super.onStop()
-        sensorManager.unregisterListener(this)
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
     }
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            when (event.sensor.type) {
-                Sensor.TYPE_ACCELEROMETER -> {
-                    accelerometerData = event.values.clone()
-                }
-                Sensor.TYPE_MAGNETIC_FIELD -> {
-                    magnetometerData = event.values.clone()
-                }
-                else -> {
-                    return
-                }
-            }
-            val rotationMatrix = FloatArray(9)
-            val rotationOK = SensorManager.getRotationMatrix(
-                rotationMatrix,
-                null,
-                accelerometerData,
-                magnetometerData
-            )
-            var rotationMatrixAdjusted = FloatArray(9)
-            when (display.rotation) {
-                Surface.ROTATION_0 -> {
-                    rotationMatrixAdjusted = rotationMatrix.clone();
-                }
-                Surface.ROTATION_90 -> {
-                    SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X,
-                        rotationMatrixAdjusted);
-                }
-                Surface.ROTATION_180 -> {
-                    SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y,
-                        rotationMatrixAdjusted);
-                }
-                Surface.ROTATION_270 -> {
-                    SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X,
-                        rotationMatrixAdjusted);
-                }
-            }
-            val orientationValues = FloatArray(3)
-            if (rotationOK) {SensorManager.getOrientation(rotationMatrixAdjusted,orientationValues)}
-            val direction = orientationValues[0]
-            val pitch = orientationValues[1]
-            val roll = orientationValues[2]
 
-            if (this::playerDirection.isInitialized)
-                playerDirection.text = getString(R.string.sensor_format, direction)
-            if (this::playerPitch.isInitialized)
-                playerPitch.text = getString(R.string.sensor_format, pitch)
-            if (this::playerRoll.isInitialized)
-                playerRoll.text = getString(R.string.sensor_format, roll)
+    override fun onAccuracyChanged(arg0: Sensor, arg1: Int) {
+        /* Intentionally left blank */
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            puckX -= event.values[0].toInt()
+            puckY += event.values[1].toInt()
         }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        /* Intentionally left blank */
+    inner class AnimatedView(context: Context?) : View(context) {
+        init {
+            goalDrawable = ShapeDrawable(OvalShape())
+            puckDrawable = ShapeDrawable(OvalShape())
+            puckDrawable.paint.color = -0x53dd
+            puckDrawable.setBounds(
+                puckX,
+                puckY,
+                puckX + Companion.puckWidth,
+                puckY + Companion.puckHeight
+            )
+            newGoal()
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            makeGoal(canvas)
+            checkBounds()
+            puckDrawable.setBounds(
+                puckX,
+                puckY,
+                puckX + Companion.puckWidth,
+                puckY + Companion.puckHeight
+            )
+            puckDrawable.draw(canvas)
+            checkGoal()
+            invalidate()
+        }
+
+        private fun newGoal() {
+            goalX = Random.nextInt(1, max_x - goalWidth - 1)
+            goalY = Random.nextInt(1, max_y - goalHeight - 1)
+        }
+
+        private fun checkGoal() {
+            val puckTop = puckY
+            val puckBot = puckY + Companion.puckHeight
+            val puckLeft = puckX
+            val puckRight = puckX + Companion.puckWidth
+            val goalTop = goalY
+            val goalBot = goalY + Companion.goalHeight
+            val goalLeft = goalX
+            val goalRight = goalX + Companion.goalWidth
+            if (puckLeft > goalLeft &&
+                puckRight < goalRight &&
+                puckTop > goalTop &&
+                puckBot < goalBot
+            )
+                newGoal()
+        }
+
+        private fun makeGoal(canvas: Canvas) {
+            goalDrawable.setBounds(
+                goalX,
+                goalY,
+                goalX + Companion.goalWidth,
+                goalY + Companion.goalHeight
+            )
+            goalDrawable.draw(canvas)
+            invalidate()
+        }
+
+        private fun checkBounds() {
+            val puckTop = puckY
+            val puckBot = puckY + Companion.puckHeight
+            val puckLeft = puckX
+            val puckRight = puckX + Companion.puckWidth
+            if (puckLeft < x) {
+                puckX = x.toInt()
+            } else if (puckRight > width) {
+                puckX = width - Companion.puckWidth
+            }
+            if (puckTop < y) {
+                puckY = y.toInt()
+            } else if (puckBot > height) {
+                puckY = height - Companion.puckHeight
+            }
+        }
+    }
+
+    companion object {
+        const val puckWidth = 50
+        const val puckHeight = 50
+        var puckX: Int = 0
+        var puckY: Int = 0
+        var max_x: Int = 0
+        var max_y: Int = 0
+        var goalX: Int = 0
+        var goalY: Int = 0
+        var goalWidth: Int = puckWidth * 4
+        var goalHeight: Int = puckHeight * 4
     }
 }
